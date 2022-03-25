e62718f34b06943977143eae841f3cd09e164d13
Hamid-0 <hamidabol200078@gmail.com>
2022-03-25 13:23:19 -0400

    fixed most errors, browser no longer closes

--- GeeGeeRegFiller.py
+++ GeeGeeRegFiller.py
@@ -1,6 +1,10 @@
+from numpy import true_divide
 from selenium import webdriver
 import time
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
 import re
 
 
@@ -8,7 +12,8 @@ import re
 loginID = "username"
 loginPIN = "pin"
 
-
+loginID='29003008039142'
+loginPIN='630877'
 
 
 user = False
@@ -37,39 +42,84 @@ def search(list, platform):
     
 
 web = webdriver.Chrome()
-
-
+    # Enters text "qwerty" with keyDown SHIFT key and after keyUp SHIFT key (QWERTYqwerty)
 
 web.get('https://geegeereg.uottawa.ca/geegeereg/Activities/ActivitiesDetails.asp?aid=316')
 
+
+
+
 try:
-    web.implicitly_wait(2)
-    login = web.find_element(by=By.XPATH, value='//*[@id="toolbar-login"]/a')
+    time.sleep(0.25)
+    login = web.find_element(By.ID, value='toolbar-login')
+    login.click()
+except:
+    time.sleep(2)
+    login = web.find_element(By.ID, value='toolbar-login')
     login.click()
 
-    
 
-    loginField = web.find_element(by=By.XPATH, value='//*[@id="ClientBarcode"]')
+time.sleep(0.25)
+try:
+    loginField = web.find_element(By.ID, value='ClientBarcode')
     loginField.send_keys(loginID)
 
-    logjnPINField = web.find_element(by=By.XPATH, value='//*[@id="AccountPIN"]')
+    logjnPINField = web.find_element(By.ID, value='AccountPIN')
     logjnPINField.send_keys(loginPIN)
 
-    loginFinal = web.find_element(by=By.XPATH, value='//*[@id="Enter"]')
+    loginFinal = web.find_element(By.ID, value='Enter')
+    loginFinal.click()
+except:
+    time.sleep(2)
+    loginField = web.find_element(By.ID, value='ClientBarcode')
+    loginField.send_keys(loginID)
+
+    logjnPINField = web.find_element(By.ID, value='AccountPIN')
+    logjnPINField.send_keys(loginPIN)
+
+    loginFinal = web.find_element(By.ID, value='Enter')
     loginFinal.click()
 
-    timesTable = web.find_element(by=By.XPATH, value='//*[@id="activity-detail-table"]')
+try:
+    time.sleep(0.25) 
+    popUpNo = web.find_element(By.XPATH,value = '//*[@id="incomplete-transaction"]/form/div[3]/table/tbody/tr/td[2]/a')
+    popUpNo.click()
+    web.get('https://geegeereg.uottawa.ca/geegeereg/Activities/ActivitiesDetails.asp?aid=316')
+except:
+    pass
 
-    nextButton = web.find_element(by=By.XPATH, value='//*[@id="activity-detail-table_next"]')
-    nextButtonClass = nextButton.get_attribute('class')
 
 
-    # allows us to check if the next button is disabled
-    nL = nextButtonClass.split(' ')
-    allSlots = []
 
-    while (not search(nL,'ui-state-disabled')): #while the next button is not disabled
-            for row in timesTable.find_elements(by=By.CSS_SELECTOR,value='tr'):
+# allows us to check if the next button is disabled
+allSlots = []
+finalPage = False
+while (not finalPage): #while the next button is not disabled
+        try:
+            time.sleep(0.25)
+            nextButton = web.find_element(By.ID, value='activity-detail-table_next')
+            nextButtonClass = nextButton.get_attribute('class')
+            # allows us to check if the next button is disabled
+            nL = nextButtonClass.split(' ')
+            timesTable = web.find_element(By.ID, value='activity-detail-table')
+            pageNums = web.find_elements(By.ID, 'activity-detail-table_paginate')
+            nums = pageNums[0].find_elements(By.CSS_SELECTOR, 'span')
+        except:
+            time.sleep(2)
+            nextButton = web.find_element(By.ID, value='activity-detail-table_next')
+            nextButtonClass = nextButton.get_attribute('class')
+            # allows us to check if the next button is disabled
+            nL = nextButtonClass.split(' ')
+            timesTable = web.find_element(By.ID, value='activity-detail-table')
+            pageNums = web.find_elements(By.ID, 'activity-detail-table_paginate')
+            nums = pageNums[0].find_elements(By.CSS_SELECTOR, 'span')
+        if nums[0].text == '23456':
+                finalPage = True
+        else:
+                
+            
+            for row in timesTable.find_elements(By.CSS_SELECTOR, 'tr'):
+                
                 rL = row.text.split('\n')
                 if (len(rL) > 2 and (rL[0] == 'WORKOUT SESSION' or rL[0] == 'EXPRESS (1 hour) WORKOUT SESSION')):
                     if(len(rL)>=6 and rL[5]=='Add'):
@@ -78,26 +128,14 @@ try:
                         infoDumpL = infoDump.split(' ')
                         slot = [infoDumpL[1], infoDumpL[2]]
                         allSlots.append(slot)
-                    
+                
+        
+        nextButtonClass = nextButton.get_attribute('class') #get the class of the next button to check if it is disabled
+        nL = nextButtonClass.split(' ') #split the class into a list of words to check if it is disabled
+        nextButton.click()
             
-            nextButtonClass = nextButton.get_attribute('class') #get the class of the next button to check if it is disabled
-            nL = nextButtonClass.split(' ') #split the class into a list of words to check if it is disabled
-            nextButton.click()
-            
 
             
-    
-except(Exception):
-    print("\nAn Error has occured please launch the program again!\n")
-    print()
-    web.quit()
-    exit()
-finally:
-    web.quit()
-
-
-
-
 chosen =False
 while(not chosen):
     print("Choose from the following times:")
@@ -105,9 +143,10 @@ while(not chosen):
     for slot in allSlots:
         i+=1
         print(i,"-",slot[0],"@ "+slot[1])
+        
     choice = input("")
 
-    if(i <10):
+    if(i < 10):
         if re.match("^([1-9])$", choice):
                 print("You have chosen:", allSlots[int(choice)-1][0],"@",allSlots[int(choice)-1][1])
                 print("Confirm? (y/n)")
@@ -118,8 +157,17 @@ while(not chosen):
                     chooseTime = allSlots[int(choice)-1][1]
         else:
             print("\nPlease enter a valid option \n")
-    else:
+    elif(i > 10):
         if re.match("^(1[1-"+str(i%10)+"])$", choice) or re.match("^([1-9])$", choice) or choice == '10':
+                print("You have chosen:", allSlots[int(choice)-1][0],"@",allSlots[int(choice)-1][1])
+                print("Confirm? (y/n)")
+                confirm = input("")
+                if confirm == 'y':
+                    chosen = True
+                    day = allSlots[int(choice)-1][0]
+                    chooseTime = allSlots[int(choice)-1][1]
+    else:
+        if re.match("^(10)$", choice) or re.match("^([1-9])$", choice) or choice == '10':
                 print("You have chosen:", allSlots[int(choice)-1][0],"@",allSlots[int(choice)-1][1])
                 print("Confirm? (y/n)")
                 confirm = input("")
@@ -133,83 +181,94 @@ while(not chosen):
 
 # ------------After Choice----------------------------------------------------------------------------------------------------
 # create a second chrome instance to fill the form
-web2 = webdriver.Chrome()
+first = web.find_element(By.ID, value='activity-detail-table_first')
+first.click()
 
-web2.get('https://geegeereg.uottawa.ca/geegeereg/Activities/ActivitiesDetails.asp?aid=316')
 
-try:
-    web2.implicitly_wait(2)
-    login = web2.find_element(by=By.XPATH, value='//*[@id="toolbar-login"]/a')
-    login.click()
+availableSlots = []
 
-    
+found = False
 
-    loginField = web2.find_element(by=By.XPATH, value='//*[@id="ClientBarcode"]')
-    loginField.send_keys(loginID)
-
-    logjnPINField = web2.find_element(by=By.XPATH, value='//*[@id="AccountPIN"]')
-    logjnPINField.send_keys(loginPIN)
-
-    loginFinal = web2.find_element(by=By.XPATH, value='//*[@id="Enter"]')
-    loginFinal.click()
-
-    timesTable = web2.find_element(by=By.XPATH, value='//*[@id="activity-detail-table"]')
-
-    nextButton = web2.find_element(by=By.XPATH, value='//*[@id="activity-detail-table_next"]')
-    nextButtonClass = nextButton.get_attribute('class')
-                
-    nL = nextButtonClass.split(' ')
-    availableSlots = []
-    allSlots = []
-    found = False
-
-    while (not search(nL,'ui-state-disabled') and not found): #while the next button is not disabled
-            for row in timesTable.find_elements(by=By.CSS_SELECTOR,value='tr'):
-                rL = row.text.split('\n')
-                if (len(rL) > 2 and (rL[0] == 'WORKOUT SESSION' or rL[0] == 'EXPRESS (1 hour) WORKOUT SESSION')):
-                    if(len(rL)>=6 and rL[5]=='Add'):
-                        infoDump = rL[2] + rL[3]
-                        infoDump = re.sub('[-]', '', infoDump)
-                        infoDumpL = infoDump.split(' ')
-                        slot = [infoDumpL[1], infoDumpL[2]]
-                        allSlots.append(slot)
-                    if infoDumpL[1] == day and infoDumpL[2] == chooseTime:
-                        availableSlots.append(slot)
-                        found = True
-                        for col in row.find_elements(by=By.CSS_SELECTOR,value='td'):
-                            for row2 in col.find_elements(by=By.CSS_SELECTOR,value='tr'):
-                                for col2 in row2.find_elements(by=By.CSS_SELECTOR,value='td'):
-                                    span = col2.find_element(by=By.TAG_NAME, value='span')
-                                    if(col2.text == 'Add' and span.text == 'Add'):
-                                        span.click()
-                                        
-            if(not found):   
-                nextButtonClass = nextButton.get_attribute('class') #get the class of the next button to check if it is disabled
-                nL = nextButtonClass.split(' ') #split the class into a list of words to check if it is disabled
-                nextButton.click()
-                
+while (not found): 
+        try:
+            time.sleep(0.25)
+            nextButton = web.find_element(By.ID, value='activity-detail-table_next')
+            nextButtonClass = nextButton.get_attribute('class')
+            # allows us to check if the next button is disabled
+            nL = nextButtonClass.split(' ')
+            timesTable2 = web.find_element(By.ID, value='activity-detail-table')
+        except:
+            time.sleep(2)
+            nextButton = web.find_element(By.ID, value='activity-detail-table_next')
+            nextButtonClass = nextButton.get_attribute('class')
+            # allows us to check if the next button is disabled
+            nL = nextButtonClass.split(' ')
+            timesTable2 = web.find_element(By.ID, value='activity-detail-table')  
+        
+        
+        for row in timesTable2.find_elements(By.CSS_SELECTOR, 'tr'):
 
             
+            rL = row.text.split('\n')
 
-    if found:
+            if (len(rL) > 2 and (rL[0] == 'WORKOUT SESSION' or rL[0] == 'EXPRESS (1 hour) WORKOUT SESSION')):
+                if(len(rL)>=6 and rL[5]=='Add'):
+                    infoDump = rL[2] + rL[3]
+                    infoDump = re.sub('[-]', '', infoDump)
+                    infoDumpL = infoDump.split(' ')
+                    slot = [infoDumpL[1], infoDumpL[2]]
+                    allSlots.append(slot)
+                if infoDumpL[1] == day and infoDumpL[2] == chooseTime:
+                    
+                    availableSlots.append(slot)
+                    found = True
+                    for col in row.find_elements(By.CSS_SELECTOR,value='td'):
+                        for row2 in col.find_elements(By.CSS_SELECTOR,value='tr'):
+                            for col2 in row2.find_elements(By.CSS_SELECTOR,value='td'):
+                                span = col2.find_element(By.TAG_NAME, value='span')
+                                if(col2.text == 'Add' and span.text == 'Add'):
+                                    span.click()
+                                    
+        if(not found):   
+            nextButtonClass = nextButton.get_attribute('class') #get the class of the next button to check if it is disabled
+            nL = nextButtonClass.split(' ') #split the class into a list of words to check if it is disabled
+            nextButton.click()
+            
+
         
 
-        checkoutBtn = web2.find_elements(by=By.XPATH, value='//*[@id="RegistrationDetails"]/div[3]/div/span/span[1]/input')
-
-        for btn in checkoutBtn:
-            if btn.is_displayed():
-                btn.click()
-                break
-        
-
-        detailBtn = web2.find_element(by=By.XPATH, value='//*[@id="regist-detail-info-btn-show"]')
-        detailBtn.click()
-    else:
-        print('Sorry this slot was just taken. Please try again')
-        web2.quit()
-except(Exception):
-    print("\nAn Error has occured please launch the program again!\n")
-    web2.quit()
-    exit()
+if found:
     
+    try:
+        time.sleep(0.25)
+        checkoutBtn = web.find_elements(By.XPATH, value='//*[@id="RegistrationDetails"]/div[3]/div/span/span[1]/input')
+    except:
+        time.sleep(2)
+        checkoutBtn = web.find_elements(By.XPATH, value='//*[@id="RegistrationDetails"]/div[3]/div/span/span[1]/input')
+    if(len(checkoutBtn) > 0):
+        checkoutBtn[0].click()
+    
+    try:
+        time.sleep(0.25)
+        detail = web.find_element(By.ID, value='regist-detail-info-btn-show')
+        detail.click()
+
+    except:
+        try:
+            time.sleep(2)
+            detail = web.find_element(By.ID, value='regist-detail-info-btn-show')
+            detail.click()
+        except:
+            print("Click Complete transaction and you are good to go!")
+            print("Enjoy Working Out!")
+    finally:
+        print("Click Complete transaction and you are good to go!")
+        print("Enjoy Working Out!")
+            
+    
+    
+else:
+    print('Sorry this slot was just taken. Please try again')
+    web.quit()
+
 
