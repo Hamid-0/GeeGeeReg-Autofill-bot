35f998c3d4f347806b78e3cab55bc8fd0501124e
Hamid-0 <hamidabol200078@gmail.com>
2022-03-25 21:30:54 -0400

    changed table logic fixed constant crash

--- GeeGeeRegFiller.py
+++ GeeGeeRegFiller.py
@@ -1,4 +1,3 @@
-from numpy import true_divide
 from selenium import webdriver
 import time
 from selenium.webdriver.common.by import By
@@ -12,8 +11,8 @@ import re
 loginID = "username"
 loginPIN = "pin"
 
-loginID='29003008039142'
-loginPIN='630877'
+
+
 
 
 user = False
@@ -42,7 +41,6 @@ def search(list, platform):
     
 
 web = webdriver.Chrome()
-    # Enters text "qwerty" with keyDown SHIFT key and after keyUp SHIFT key (QWERTYqwerty)
 
 web.get('https://geegeereg.uottawa.ca/geegeereg/Activities/ActivitiesDetails.asp?aid=316')
 
@@ -91,58 +89,31 @@ except:
 
 
 
-# allows us to check if the next button is disabled
 allSlots = []
-finalPage = False
-while (not finalPage): #while the next button is not disabled
-        try:
-            time.sleep(0.25)
-            nextButton = web.find_element(By.ID, value='activity-detail-table_next')
-            nextButtonClass = nextButton.get_attribute('class')
-            # allows us to check if the next button is disabled
-            nL = nextButtonClass.split(' ')
-            timesTable = web.find_element(By.ID, value='activity-detail-table')
-            pageNums = web.find_elements(By.ID, 'activity-detail-table_paginate')
-            nums = pageNums[0].find_elements(By.CSS_SELECTOR, 'span')
-        except:
-            time.sleep(4)
-            nextButton = web.find_element(By.ID, value='activity-detail-table_next')
-            nextButtonClass = nextButton.get_attribute('class')
-            # allows us to check if the next button is disabled
-            nL = nextButtonClass.split(' ')
-            timesTable = web.find_element(By.ID, value='activity-detail-table')
-            pageNums = web.find_elements(By.ID, 'activity-detail-table_paginate')
-            nums = pageNums[0].find_elements(By.CSS_SELECTOR, 'span')
-        if nums[0].text == '23456':
-                finalPage = True
-        else:
-                
-            
-            for row in timesTable.find_elements(By.CSS_SELECTOR, 'tr'):
-                
-                try:
-                    rL = row.text.split('\n')
-                except:
-                    try:
-                        time.sleep(0.25)
-                        rL = row.text.split('\n')
-                    except:
-                        time.sleep(4)
-                        rL = row.text.split('\n')
-                    
-                print(rL)
-                if (len(rL) > 2 and (rL[0] == 'WORKOUT SESSION' or rL[0] == 'EXPRESS (1 hour) WORKOUT SESSION')):
-                    if(len(rL)>=6 and rL[5]=='Add'):
-                        infoDump = rL[2] + rL[3]
-                        infoDump = re.sub('[-]', '', infoDump)
-                        infoDumpL = infoDump.split(' ')
-                        slot = [infoDumpL[1], infoDumpL[2]]
-                        allSlots.append(slot)
-                
-        
-        nextButtonClass = nextButton.get_attribute('class') #get the class of the next button to check if it is disabled
-        nL = nextButtonClass.split(' ') #split the class into a list of words to check if it is disabled
-        nextButton.click()
+for i in range(0,4):
+
+    time.sleep(0.25)
+    nextButton = web.find_element(By.ID, value='activity-detail-table_next')      
+    sessionType = web.find_elements(By.XPATH, value = '//*[@id="activity-course-row"]/td[1]')
+    days = web.find_elements(By.XPATH, value = '//*[@id="activity-course-row"]/td[3]')
+    times = web.find_elements(By.XPATH, value = '//*[@id="activity-course-row"]/td[4]')
+    locations = web.find_elements(By.XPATH, value = '//*[@id="activity-course-row"]/td[6]')
+    addOrNot = web.find_elements(By.XPATH, value = '//*[@id="activity-course-row"]/td[9]/table/tbody/tr[1]/td/span')
+    for j in range(len(sessionType)):
+        sessionTypeF = sessionType[j].text.split('\n')[0]
+        timeF= times[j].text.strip(" ").split('-')[0]
+        if sessionTypeF =='EXPRESS (1 hour) WORKOUT SESSION' or sessionTypeF =='WORKOUT SESSION':
+            if addOrNot[j].text == 'Add':
+                allSlots.append([sessionTypeF, days[j].text, timeF, locations[j].text, addOrNot[j].text])
+
+    nextButton.click()
+
+
+
+time.sleep(0.25)
+
+
+
             
 
             
@@ -152,39 +123,39 @@ while(not chosen):
     i= 0
     for slot in allSlots:
         i+=1
-        print(i,"-",slot[0],"@ "+slot[1])
-        
+        # print(i,"-",slot[1],"@"+slot[2] + "   in " + slot[3])
+        print(f"{i}{' - ':2s}{slot[1]:2s}{' @'+slot[2]:10s}{'in':3s}{slot[3]:30s}")
     choice = input("")
 
     if(i < 10):
         if re.match("^([1-9])$", choice):
-                print("You have chosen:", allSlots[int(choice)-1][0],"@",allSlots[int(choice)-1][1])
+                print(f"{allSlots[int(choice)-1][1]:2s}{' @'+allSlots[int(choice)-1][2]:8s}{'in':3s}{allSlots[int(choice)-1][3]:30s}")
                 print("Confirm? (y/n)")
                 confirm = input("")
                 if confirm == 'y':
                     chosen = True
-                    day = allSlots[int(choice)-1][0]
-                    chooseTime = allSlots[int(choice)-1][1]
+                    chooseDay = allSlots[int(choice)-1][1]
+                    chooseTime = allSlots[int(choice)-1][2]
         else:
             print("\nPlease enter a valid option \n")
     elif(i > 10):
         if re.match("^(1[1-"+str(i%10)+"])$", choice) or re.match("^([1-9])$", choice) or choice == '10':
-                print("You have chosen:", allSlots[int(choice)-1][0],"@",allSlots[int(choice)-1][1])
+                print(f"{allSlots[int(choice)-1][1]:2s}{' @'+allSlots[int(choice)-1][2]:8s}{'in':3s}{allSlots[int(choice)-1][3]:30s}")
                 print("Confirm? (y/n)")
                 confirm = input("")
                 if confirm == 'y':
                     chosen = True
-                    day = allSlots[int(choice)-1][0]
-                    chooseTime = allSlots[int(choice)-1][1]
+                    chooseDay = allSlots[int(choice)-1][1]
+                    chooseTime = allSlots[int(choice)-1][2]
     else:
         if re.match("^(10)$", choice) or re.match("^([1-9])$", choice) or choice == '10':
-                print("You have chosen:", allSlots[int(choice)-1][0],"@",allSlots[int(choice)-1][1])
+                print(f"{allSlots[int(choice)-1][1]:2s}{' @'+allSlots[int(choice)-1][2]:8s}{'in':3s}{allSlots[int(choice)-1][3]:30s}")
                 print("Confirm? (y/n)")
                 confirm = input("")
                 if confirm == 'y':
                     chosen = True
-                    day = allSlots[int(choice)-1][0]
-                    chooseTime = allSlots[int(choice)-1][1]
+                    chooseDay = allSlots[int(choice)-1][1]
+                    chooseTime = allSlots[int(choice)-1][2]
         else:
             print("\nPlease enter a valid option \n")
         
@@ -194,100 +165,54 @@ while(not chosen):
 first = web.find_element(By.ID, value='activity-detail-table_first')
 first.click()
 
-
-availableSlots = []
-
 found = False
 
-while (not found): 
-        try:
-            time.sleep(0.25)
-            nextButton = web.find_element(By.ID, value='activity-detail-table_next')
-            nextButtonClass = nextButton.get_attribute('class')
-            # allows us to check if the next button is disabled
-            nL = nextButtonClass.split(' ')
-            timesTable2 = web.find_element(By.ID, value='activity-detail-table')
-        except:
-            time.sleep(4)
-            nextButton = web.find_element(By.ID, value='activity-detail-table_next')
-            nextButtonClass = nextButton.get_attribute('class')
-            # allows us to check if the next button is disabled
-            nL = nextButtonClass.split(' ')
-            timesTable2 = web.find_element(By.ID, value='activity-detail-table')  
-        
-        
-        for row in timesTable2.find_elements(By.CSS_SELECTOR, 'tr'):
+while(not found):
 
-            try:
-                rL = row.text.split('\n')
-            except:
-                try:
-                    time.sleep(0.25)
-                    rL = row.text.split('\n')
-                except:
-                    time.sleep(4)
-                    rL = row.text.split('\n')
-
-
-            if (len(rL) > 2 and (rL[0] == 'WORKOUT SESSION' or rL[0] == 'EXPRESS (1 hour) WORKOUT SESSION')):
-                if(len(rL)>=6 and rL[5]=='Add'):
-                    infoDump = rL[2] + rL[3]
-                    infoDump = re.sub('[-]', '', infoDump)
-                    infoDumpL = infoDump.split(' ')
-                    slot = [infoDumpL[1], infoDumpL[2]]
-                    allSlots.append(slot)
-                if infoDumpL[1] == day and infoDumpL[2] == chooseTime:
-                    
-                    availableSlots.append(slot)
-                    found = True
-                    for col in row.find_elements(By.CSS_SELECTOR,value='td'):
-                        for row2 in col.find_elements(By.CSS_SELECTOR,value='tr'):
-                            for col2 in row2.find_elements(By.CSS_SELECTOR,value='td'):
-                                span = col2.find_element(By.TAG_NAME, value='span')
-                                if(col2.text == 'Add' and span.text == 'Add'):
-                                    span.click()
-                                    
-        if(not found):   
-            nextButtonClass = nextButton.get_attribute('class') #get the class of the next button to check if it is disabled
-            nL = nextButtonClass.split(' ') #split the class into a list of words to check if it is disabled
-            nextButton.click()
-            
-
-        
-
-if found:
-    try:
-        time.sleep(0.25)
-        checkoutBtn = web.find_elements(By.XPATH, value='//*[@id="RegistrationDetails"]/div[3]/div/span/span[1]/input')
-    except:
-        time.sleep(2)
-        checkoutBtn = web.find_elements(By.XPATH, value='//*[@id="RegistrationDetails"]/div[3]/div/span/span[1]/input')
-    if(len(checkoutBtn) >= 0):
-        checkoutBtn[0].click()
-    else:
-        checkoutBtn.Key.ENTER
+    time.sleep(0.25)
+    nextButton = web.find_element(By.ID, value='activity-detail-table_next')      
+    sessionType = web.find_elements(By.XPATH, value = '//*[@id="activity-course-row"]/td[1]')
+    days = web.find_elements(By.XPATH, value = '//*[@id="activity-course-row"]/td[3]')
+    times = web.find_elements(By.XPATH, value = '//*[@id="activity-course-row"]/td[4]')
+    locations = web.find_elements(By.XPATH, value = '//*[@id="activity-course-row"]/td[6]')
+    addOrNot = web.find_elements(By.XPATH, value = '//*[@id="activity-course-row"]/td[9]/table/tbody/tr[1]/td/span')
+    for j in range(len(sessionType)):
+        sessionTypeF = sessionType[j].text.split('\n')[0]
+        timeF= times[j].text.strip(" ").split('-')[0]
+        if days[j].text == chooseDay and timeF == chooseTime:
+            addOrNot[j].click()
+            found = True
     
+    if not found:
+        nextButton.click()
+
+
+
+time.sleep(1)
+checkoutBtn = web.find_element(By.XPATH, value='//*[@id="RegistrationDetails"]/div[3]/div/span/span[1]/input')
+
+checkoutBtn.click()
+    
+    
+
+try:
+    time.sleep(0.25)
+    detail = web.find_element(By.ID, value='regist-detail-info-btn-show')
+    detail.click()
+
+except:
     try:
-        time.sleep(0.25)
+        time.sleep(2)
         detail = web.find_element(By.ID, value='regist-detail-info-btn-show')
         detail.click()
-
     except:
-        try:
-            time.sleep(2)
-            detail = web.find_element(By.ID, value='regist-detail-info-btn-show')
-            detail.click()
-        except:
-            print("Click Complete transaction and you are good to go!")
-            print("Enjoy Working Out!")
-    finally:
         print("Click Complete transaction and you are good to go!")
         print("Enjoy Working Out!")
-            
-    
-    
-else:
-    print('Sorry this slot was just taken. Please try again')
-    web.quit()
+finally:
+    print("Click Complete transaction and you are good to go!")
+    print("Enjoy Working Out!")
+        
+
+
 
 
